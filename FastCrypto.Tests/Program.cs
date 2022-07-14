c2c0ac1780a3478aab08162d8a93b77450b0de75
Akseli <akseli.laine201@gmail.com>
2022-07-14 06:34:50 +0300

    Add project files.

---
+++ FastCrypto.Tests/Program.cs
@@ -0,0 +1,48 @@
+﻿
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using FastCrypto;
+using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.Crypto.Parameters;
+using System.Text;
+
+BenchmarkRunner.Run<CryptoTest>();
+Console.ReadKey();
+
+public class CryptoTest {
+
+    static byte[] EncryptionKey = { 200, 100, 18, 164, 146, 232, 122, 183, 58, 214, 130, 177, 50 };
+
+    // FastCrypto rc4 implementation
+    RC4 _fastRC4;
+
+    // BouncyCastle rc4 implementation
+    RC4Engine _bouncyRC4;
+
+    // seperate blocks with the same data (obviously)
+    byte[] _bytes1;
+    byte[] _bytes2;
+
+    public CryptoTest() {
+
+        // init fast rc4
+        _fastRC4 = new RC4(EncryptionKey);
+
+        // init bouncy rc4
+        _bouncyRC4 = new RC4Engine();
+        _bouncyRC4.Init(true, new KeyParameter(EncryptionKey));
+
+        _bytes1 = new byte[1024];
+        _bytes2 = new byte[1024];
+    }
+
+    [Benchmark]
+    public void FastCrypto_RC4() {
+        _fastRC4.ProcessBytes(_bytes1, 0, _bytes1.Length);
+    }
+
+    [Benchmark]
+    public void BouncyCastle_RC4() {
+        _bouncyRC4.ProcessBytes(_bytes2, 0, _bytes2.Length, _bytes2, 0);
+    }
+}
---
+++ FastCrypto/RC4.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastCrypto {
+
+    /// <summary>
+    /// Optimized RC4 Cipher for processing byte arrays
+    /// </summary>
+    public class RC4 {
+
+        private const int StateLength = 256;
+
+        private byte[] _state;
+        private byte[] _key;
+        private int _x;
+        private int _y;
+
+        public RC4(byte[] key) {
+            _key = key;
+            _state = new byte[StateLength];
+            SetKey(key);
+        }
+
+        /// <summary>
+        /// Resets the cipher to its original state
+        /// </summary>
+        public void Reset() {
+            SetKey(_key);
+        }
+
+        /// <summary>
+        /// Encrypts a block of bytes
+        /// </summary>
+        /// <param name="input">The block of bytes to encrypt</param>
+        /// <param name="length">The amount of bytes to encrypt</param>
+        /// <param name="offset">The offset in the buffer where to start encrypting</param>
+        /// <exception cref="ArgumentOutOfRangeException">If the input buffer is too short</exception>
+        public void ProcessBytes(byte[] input, int offset, int length) {
+
+            if (length + offset > input.Length) {
+                throw new ArgumentOutOfRangeException("input buffer is too short");
+            }
+
+            unsafe {
+                // pin both, the engine state and input
+                // to local fixed buffers
+                fixed (byte* engine = _state, pointer = input) {
+
+                    byte* ptr = (pointer + offset);
+
+                    // create local copies of the x,y states
+                    // because accessing locals is faster
+                    int x = _x;
+                    int y = _y;
+
+                    // reverse loop is faster because the length needs to be read only once
+                    for (int i = length; i > 0; i--) {
+
+                        // optimized operations
+                        // removed unnecessary array accesses
+
+                        byte xState = engine[x = (x + 1) & 0xff];
+                        byte yState = engine[y = (xState + y) & 0xff];
+
+                        engine[x] = yState;
+                        engine[y] = xState;
+
+                        *ptr ^= engine[(yState + xState) & 0xff];
+
+                        // point to the next index
+                        ptr++;
+                    }
+
+                    // return the state
+                    _x = x;
+                    _y = y;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Sets the state to a given key
+        /// </summary>
+        /// <param name="keyBytes"></param>
+        private void SetKey(byte[] keyBytes) {
+            
+            _x = _y = 0;
+
+            for (int i = 0; i < StateLength; i++) {
+                _state[i] = (byte)i;
+            }
+
+            for (int i = 0, j = 0, k = 0; i < StateLength; i++) {
+                byte tmp = _state[i];
+                k = ((keyBytes[j] & 0xff) + tmp + k) & 0xff;
+                _state[i] = _state[k];
+                _state[k] = tmp;
+                j = (j + 1) % keyBytes.Length;
+            }
+        }
+    }
+}
